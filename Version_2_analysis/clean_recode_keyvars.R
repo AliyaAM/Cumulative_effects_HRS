902ebfd480f452c338a57a207a0913c34b433e91
AliyaAM <aliya.amirova005@gmail.com>
2022-04-13 20:01:25 +0100

    changed cvd so we have 0, 1, NA

--- Version_2_analysis/clean_recode_keyvars.R
+++ Version_2_analysis/clean_recode_keyvars.R
@@ -63,9 +63,24 @@ clean_recode_keyvars =  function (data){
   
   ##### recode below: 
   
-  data$CVD[data$heartcondition_ever_bin == 1 | data$heartcondition_new_bin == 1 | data$angina_new_bin ==1 | data$stroke_new_bin == 1 | data$heartfailure2yrs_bin == 1 | data$heartattack_ever_bin == 1 | data$heartattack_new_bin == 1] <-1
-  data$CVD[data$heartcondition_ever_bin == 0 & data$heartcondition_new_bin == 0 & data$angina_new_bin ==0 & data$stroke_new_bin == 0 & data$heartfailure2yrs_bin == 0 & data$heartattack_ever_bin == 0 & data$heartattack_new_bin == 0] <-0
-
+  data$CVD[data$angina_new_bin ==1 | data$heartfailure2yrs_bin == 1 | data$heartattack_ever_bin == 1 | data$heartattack_new_bin == 1] <-1
+  data$CVD[data$angina_new_bin ==0 & data$heartfailure2yrs_bin == 0 & data$heartattack_ever_bin == 0 & data$heartattack_new_bin == 0] <-0
+  
+  unique(data$CVD)
+  
+  
+  data$CVD_ever[data$heartfailure2yrs_bin == 1 | data$heartattack_ever_bin == 1 ] <-1
+  data$CVD_ever[data$heartfailure2yrs_bin == 0 & data$heartattack_ever_bin == 0 ] <-0
+  
+  unique(data$CVD_ever)
+  
+  
+  data$CVD_new[data$angina_new_bin ==1 | data$heartattack_new_bin == 1] <-1
+  data$CVD_new[data$angina_new_bin ==0 & data$heartattack_new_bin == 0] <-0
+  
+  
+  
+  unique(data$CVD_new)
 
 
 
