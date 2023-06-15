ed35f63c444f7be3151d090eced846aafd2b04b0
AliyaAM <aliya.amirova005@gmail.com>
2023-06-15 19:16:56 +0100

    excluding people with cardiomet disease

--- Reviewer_response/Version_2_analysis/Plotting_surv_Model_1_exclude_cardio_met_add_new_cov.R
+++ Reviewer_response/Version_2_analysis/Plotting_surv_Model_1_exclude_cardio_met_add_new_cov.R
@@ -1,7 +1,8 @@
 
-# plots: 
-# https://adibender.github.io/pammtools/articles/cumulative_effects.html
-#https://rpkgs.datanovia.com/ggpubr/reference/stat_regline_equation.html Adding text (coefficients etc) to the plot next to the regression line 
+library("survival")
+library("survminer")
+library("dplyr")
+
 
 current_directory = "/Users/k2147340/OneDrive - King's College London/Documents/"
 
@@ -14,27 +15,13 @@ DATA_ROOT = paste(current_directory, "/ELSA_HRS/Data_analysis/", sep = "")
 
 
 
-library("survival")
-library("survminer")
-library("dplyr")
-library("ggplot2")
+#### "education_level", "national_origin_ousideUS_bin", "race_white"
 
 
-#Model 1: age and sex, wealth  [basis adjustment]
-Model_1 = c("continious_age", "wealth_noIRA", "sex_1_2")
-#Model 2: age, sex, wealth, BMI, hypertension  [basic adjustment + diabetes risk factors]
-Model_2 = c("continious_age", "wealth_noIRA", "sex_1_2", "assessed_BMI", "hypertension_new_bin")
-#Model 3: age, sex, wealth, physical activity, smoking (yes/no), and alcohol (days/week) [basic adjustment + health behaviours]
-Model_3 = c("continious_age", "wealth_noIRA", "sex_1_2","alcohol_days_week_new",  "vigarious_physical_activity_new", 'smokes_now_bin')
-#Model 4: age, sex, wealth, CVD  [basic adjustment + CVD most common diabetes co_morbidity]
-Model_4 = c("continious_age", "wealth_noIRA", "sex_1_2")
-#Model 5: age, sex, wealth, depression  [basic adjustment + depression best researched psychosocial factor in diabetes ]
-Model_5 = c("continious_age","wealth_noIRA", "sex_1_2","checklist_depression_bin")
-#Model 6: age, sex, wealth, BMI, hypertension, CVD  [basic adjustment + diabetes risk factors+ CVD]
-Model_6 = c("continious_age", "wealth_noIRA", "sex_1_2", "assessed_BMI", "hypertension_new_bin")
-#Model 7: age, sex, wealth, BMI, hypertension, depression  [basic adjustment + diabetes risk factors+ Depression]
-Model_7 = c("continious_age", "wealth_noIRA", "sex_1_2", "assessed_BMI", "hypertension_new_bin", "checklist_depression_bin")
 
+
+
+###### Adding variables to the main dataset:
 
 
 ###### DATA:
@@ -53,22 +40,13 @@ exclude_ids = unique(cases_with_CVD$HHIDPN)
 
 cumulative_effects_dat <- subset(cumulative_effects_dat_initial,  !(HHIDPN %in% exclude_ids))
 
- 
+
 unique(cumulative_effects_dat$CVD_ever)
 unique(cumulative_effects_dat_initial$start_new)
 nrow(cumulative_effects_dat)
 nrow(cumulative_effects_dat_initial)
 
 
-
-###### Adding variables to the main dataset:
-
-#outcome:  cumulative_effects_dat$diabetes_new_bin
-#exposure: cumulative_effects_dat$discrim_bin
-#time: cumulative_effects_dat$stop_new
-
-###### Adding variables to the main dataset:
-###### Adding variables to the main dataset:
 
 
 
@@ -142,15 +120,16 @@ cumulative_effects_dat$discrim_bin = case_when(cumulative_effects_dat$discrim_ha
 
 
 
+#outcome:  cumulative_effects_dat$diabetes_new_bin
+#exposure: cumulative_effects_dat$discrim_bin
+#time: cumulative_effects_dat$stop_new
 
-cumulative_effects_dat$discrimination = cumulative_effects_dat$discrim_bin 
+###### Adding variables to the main dataset:
+###### Adding variables to the main dataset:
 
+cumulative_effects_dat$discrim_bin
 
-
-cumulative_effects_dat$discrimination_reversed = case_when(cumulative_effects_dat$discrim_bin == 1 ~ 0,
-                                                           cumulative_effects_dat$discrim_bin == 0 ~ 1)
-
-unique(cumulative_effects_dat$start_new)
+cumulative_effects_dat$discrimination = cumulative_effects_dat$discrim_bin
 
 cumulative_effects_dat$time_point = cumulative_effects_dat$start_new
 
@@ -161,21 +140,36 @@ cumulative_effects_dat$months = 12 * cumulative_effects_dat$years
 cumulative_effects_dat$follow_up = cumulative_effects_dat$years
 
 
-#1 = 2 year 
-#2 = 4 follow_up 
-#3 = 6 follow_up 
-
-
-unique(cumulative_effects_dat$start_new)
-
-sd(cumulative_effects_dat$start_new)
-
 cumulative_effects_dat$diabetes_new_bin = case_when(cumulative_effects_dat$diabetes_new == 1 ~ 1, 
                                                     cumulative_effects_dat$diabetes_new == 0 ~ 0) 
 
 
 cumulative_effects_dat$diabetes_new_bin_reversed = case_when(cumulative_effects_dat$diabetes_new_bin == 1 ~ 0, 
                                                              cumulative_effects_dat$diabetes_new_bin == 0 ~ 1) 
+
+
+#outcome:  cumulative_effects_dat$diabetes_new_bin
+#exposure: cumulative_effects_dat$discrim_bin
+#time: cumulative_effects_dat$stop_new
+
+###### Adding variables to the main dataset:
+###### Adding variables to the main dataset:
+
+unique(cumulative_effects_dat$discrimination_cat)
+
+
+#1 = 2 year 
+#2 = 4 years 
+#3 = 6 years 
+
+#coxph(Surv(t1, t2, stat) ∼ (age + surgery)* transplant) – time dependent covariates.
+
+unique(cumulative_effects_dat$timepoints_indiv)
+unique(cumulative_effects_dat$start_new)
+
+cumulative_effects_dat$diabetes_new_bin_reversed = case_when(cumulative_effects_dat$diabetes_new_bin == 1 ~ 0, 
+                                                             cumulative_effects_dat$diabetes_new_bin == 0 ~ 1) 
+
 
 
 #cumulative_effects_dat$sex_1_2
@@ -186,77 +180,36 @@ cumulative_effects_dat$diabetes_new_bin_reversed = case_when(cumulative_effects_
 #cumulative_effects_dat$assessed_BMI
 
 
-#### plot for the entire dataset: 
+#### plot for the entire dataset:
+#survfit.coxph
 
-fit <- survfit(Surv(follow_up, diabetes_new_bin) ~ discrimination, data = cumulative_effects_dat)
+#cfit <- coxph(Surv(futime, death) ~ sex + age*hgb, data=mgus2)
+
+fit <- coxph(Surv(follow_up, diabetes_new_bin)~ discrimination + continious_age + wealth_noIRA + sex_1_2 + race_white, data = cumulative_effects_dat)
 summary_all = summary(fit)
-print(summary_all)
 
-
-### output below: 
-
-summary_all_table = summary_all$table
-subset_all = rep("All", time=nrow(summary_all_table))
-summary_all_table = cbind(subset_all, summary_all_table)
-
-write.csv(summary_all_table, paste(OUTPUT_ROOT, "summary_all_table_discrimination_cat_no_cardiomet_disease.csv")) 
-
-Number_events_all = fit$n.event
-Number_risk_all = fit$n.risk
-Number_censored_all = fit$n.censor
-probability_for_nodiab_all = fit$surv
-probability_SE_all = fit$std.err
-probability_lower_CI_all = fit$lower
-probability_upper_CI_all = fit$upper
-cumhaz_output_all =  fit$cumhaz
-cumhaz_output_std_all = fit$std.chaz
-
-
+# coeffcients for discrimination: 
+summary_all$coefficients[1,]
+# exp (HR), and 95% CI: 
+summary_all$conf.int[1,]
+summary_all$waldtest
+summary_all$logtest[1]
+summary_all$n
+summary_all$nevent
 
 
 ####
 ### output below: 
-All_results_unadjusted = cbind(Number_events_all,
-                               Number_risk_all,
-                               Number_censored_all, 
-                               probability_for_nodiab_all, 
-                               probability_SE_all, 
-                               probability_lower_CI_all, 
-                               probability_upper_CI_all, 
-                               cumhaz_output_all, 
-                               cumhaz_output_std_all) 
+All_results_Model_1 = data.frame("Model_1")
+All_results_Model_1$subset  = c("All")
+All_results_Model_1$coef  = c(summary_all$conf.int[1,1])
+All_results_Model_1$lower_CI = c(summary_all$conf.int[1,3])
+All_results_Model_1$upper_CI = c(summary_all$conf.int[1,4])
+All_results_Model_1$logtest = summary_all$logtest[1]
+All_results_Model_1$df = summary_all$logtest[2]
+All_results_Model_1$p_value = summary_all$logtest[3]
 
-subset_all_undj  = rep("All", time=nrow(All_results_unadjusted)) 
-All_results_unadjusted = cbind(subset_all_undj, All_results_unadjusted) 
-write.csv(All_results_unadjusted, paste(OUTPUT_ROOT, "All_results_unadjusted_discrimination_cat_no_cardiomet_disease.csv", sep = "")) 
+print(All_results_Model_1)
 
-
-plot_all = ggsurvplot(fit,
-                      pval = TRUE, conf.int = TRUE,
-                      risk.table = FALSE, # Add risk table
-                      risk.table.col = "strata", # Change risk table color by groups
-                      linetype = "strata", # Change line type by groups
-                      surv.median.line = "hv", # Specify median survival
-                      
-                      surv.scale = "percent",
-                      
-                      ggtheme = theme_bw(),
-                      ylim = c(0.5, 1),
-                      pval.coord = c(0, 0.95),
-                      
-                      xlab = "Years from baseline", 
-                      
-                      ylab = "proportion of cases free of diabetes", 
-                      
-                      # Change ggplot2 theme
-                      palette = c("#E7B800", "#2E9FDF"))
-
-plot_all
-print(plot_all)  
-
-
-########
-########
-########
-########
+write.csv(All_results_Model_1, paste(OUTPUT_ROOT, "All_results_Model_1_exclude_cardiomet_disease_add_cov_race.csv"))
 
