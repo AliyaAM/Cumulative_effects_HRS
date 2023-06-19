46b087f3de2838c3df5bca476f8d11900ba3c5c4
AliyaAM <aliya.amirova005@gmail.com>
2023-06-19 17:33:42 +0100

    Update flow_chart_drop_baseline_diabetes.R

--- Reviewer_response/Version_2_analysis/flow_chart_drop_baseline_diabetes.R
+++ Reviewer_response/Version_2_analysis/flow_chart_drop_baseline_diabetes.R
@@ -152,49 +152,54 @@ flow_chart_drop_baseline_diabetes = function (data){
   #wave 3: 233 cases 
   
   
-  wave_4 = subset(participant_wave_df, participant_wave_df$start_new == 3)
+  #wave_4 = subset(participant_wave_df, participant_wave_df$start_new == 3)
   #wave_4_diabetes  = subset(wave_4, wave_4$diabetes_new == 1)
   
   
-  diabetes_wave_4_unique = unique(wave_4_diabetes$HHIDPN)
-  new_diabetes_wave4_dataset = length(diabetes_wave_4_unique)
+  #diabetes_wave_4_unique = unique(wave_4_diabetes$HHIDPN)
+  #new_diabetes_wave4_dataset = length(diabetes_wave_4_unique)
   #wave 4: 174 cases 
   
   
-  wave_5 = subset(participant_wave_df, participant_wave_df$start_new == 4)
-  wave_5_diabetes  = subset(wave_5, wave_5$diabetes_new == 1)
+  #wave_5 = subset(participant_wave_df, participant_wave_df$start_new == 4)
+  #wave_5_diabetes  = subset(wave_5, wave_5$diabetes_new == 1)
   
   
-  diabetes_wave_5_unique = unique(wave_5_diabetes$HHIDPN)
-  new_diabetes_wave5_dataset = length(diabetes_wave_5_unique)
+  #diabetes_wave_5_unique = unique(wave_5_diabetes$HHIDPN)
+  #new_diabetes_wave5_dataset = length(diabetes_wave_5_unique)
   #
   
-  wave_6 = subset(participant_wave_df, participant_wave_df$start_new == 5)
-  wave_6_diabetes  = subset(wave_6, wave_6$diabetes_new == 1)
+  #wave_6 = subset(participant_wave_df, participant_wave_df$start_new == 5)
+  #wave_6_diabetes  = subset(wave_6, wave_6$diabetes_new == 1)
   
   
-  diabetes_wave_6_unique = unique(wave_6_diabetes$HHIDPN)
-  new_diabetes_wave6_dataset = length(diabetes_wave_6_unique)
+  #diabetes_wave_6_unique = unique(wave_6_diabetes$HHIDPN)
+  #new_diabetes_wave6_dataset = length(diabetes_wave_6_unique)
   
   
   
   new_diabetes_each_wave = rbind(new_diabetes_wave2_dataset,
-   new_diabetes_wave3_dataset,
-   new_diabetes_wave4_dataset,
-   new_diabetes_wave5_dataset,
-   new_diabetes_wave6_dataset)
+   new_diabetes_wave3_dataset) 
+   #new_diabetes_wave4_dataset,
+   #new_diabetes_wave5_dataset,
+   #new_diabetes_wave6_dataset)
   
-  #write.csv(new_diabetes_each_wave, (paste(OUTPUT_ROOT, "new_diabetes_each_wave_DIAB.csv", sep="")))
+  write.csv(new_diabetes_each_wave, (paste(OUTPUT_ROOT, "new_diabetes_each_wave_DIAB.csv", sep="")))
   
   all_waves = rbind(wave_1, 
                     wave_2, 
-                    wave_3, 
-                    wave_4, 
-                    wave_5, 
-                    wave_6)
+                    wave_3) 
+                    #wave_4, 
+                    #wave_5, 
+                    #wave_6)
   
   
   
-  return(new_diabetes_each_wave)
+  return(all_waves)
   
 }
+
+
+data_flow_chart = flow_chart_drop_baseline_diabetes(data = analytical_sample_COX) 
+
+write.csv(data_flow_chart, file = (paste(OUTPUT_ROOT, "data_flow_chart.csv", sep="")))
